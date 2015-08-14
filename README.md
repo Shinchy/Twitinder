91e268b6c183769d3f201507ae3bb0dd548e6731
Shaun Hinchy <shinchy@hotmail.com>
2015-08-14 11:17:29 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Twitinder Game
+
+A silly little app to match up Twitter Users based on Similar Likes.
+
+If you want to play around with it, go ahead. Just install it using the 'npm install' method to get it up and running.
+
+## Dependancies
+
+There is only one dependency, thats the Bootstrap CSS file. 
+
+There is a Gulp file that runs some simple tasks:
+
+- SASS File, just for the basics.
+- Javascript, just to convert any ES6 features I use over with Babel.
+- Browsersync, just for that lovely ease of use.
+
+
+
+## Notes
+
+The project is all built within the folder: **app**
+
+The project is all output to the folder: **client**
---
+++ tests/game.spec.js
@@ -0,0 +1,37 @@
+// Test Suite for the Revlon Game
+// 
+
+describe("Game Test Units", function(){
+
+	// Before hand lets load in the Module
+	beforeEach(function(){
+		var game = Object.create();
+	});
+
+
+	it("Should discard a disliked card", function() {
+		//- Test
+	});
+
+	it("Should add a liked card to the array", function() {
+		//- Test
+	});
+
+	it("should show a new card once one has been used", function() {
+		// Test
+	});
+
+	it("Should end after 5 cards are liked", function() {
+		// Test
+	});
+
+	it("Should respond with a message when the cards have run out", function() {
+		// Test
+	});
+
+	it("Should reset all variables and values on a restart", function() {
+
+	});
+
+
+});
